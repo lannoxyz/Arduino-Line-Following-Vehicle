7adb80423f7da7c3ff8443d0ab5fd4bc7d3eaac8
oscarcgw <oscarhugogo@gmail.com>
2026-04-20 20:26:10 +0800

    Update test.py

--- sem2/week1/test.py
+++ sem2/week1/test.py
@@ -1,19 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-# ====== GPIO 引脚 ======
-# 左轮
+# ====== GPIO pins ======
+# left motor
 IN1 = 11
 IN2 = 13
 ENA = 32
 
-# 右轮
+# right motor
 IN3 = 29
 IN4 = 31
 ENB = 33
 
-# ====== GPIO 设置 ======
-GPIO.setmode(GPIO.BOARD)  # 使用板子引脚编号
+# ====== GPIO configuration ======
+GPIO.setmode(GPIO.BOARD)  # following board mode on pins naming
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(ENA, GPIO.OUT)
@@ -23,9 +23,9 @@ GPIO.setup(IN4, GPIO.OUT)
 GPIO.setup(ENB, GPIO.OUT)
 
 # ====== PWM setup ======
-left_pwm = GPIO.PWM(ENA, 500)   # 500 Hz
+left_pwm = GPIO.PWM(ENA, 500)   # 500Hz pwm frequency
 right_pwm = GPIO.PWM(ENB, 500)
-left_pwm.start(50)
+left_pwm.start(50) # 50% duty cycle
 right_pwm.start(50)
 
 # ====== forward ======
@@ -34,9 +34,9 @@ def forward(duration=30, speed=100):
     GPIO.output(ENA, 255)
     GPIO.output(ENB, 255)
     GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW) # left motor
     GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW) # right motor
     time.sleep(duration)
     print("Stopped")
 
